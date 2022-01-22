624534001848c5a474aa45c0e64f923653fc5338
Suraj vishwakarma <90332766+vishwakarmasuraj@users.noreply.github.com>
2022-01-22 17:32:14 +0530

    feat: Done
    feat: Done

--- src/validation/userValidRule.js
+++ src/validation/userValidRule.js
@@ -1,10 +1,10 @@
-import { body } from "express-validator";
+import { check } from "express-validator";
 import { userModel } from "../models";
 
 export const userValidRule = () => {
     return [
-        body('fullName').notEmpty().withMessage('Please enter full name').isAlpha('en-US', {ignore: ' '}).withMessage('Full name should be alpha char!'),
-        body('email').notEmpty().withMessage('Please enter email').isEmail().withMessage('Email should be email type')
+        check('fullName').isAlpha('en-US', {ignore: ' '}).withMessage('Full name should be alpha char!'),
+        check('email').isEmail().withMessage('Email should be email type')
         .custom(value => {
             return userModel.findOne({email: value}).then(data => {
                 if (data) {
@@ -12,6 +12,6 @@ export const userValidRule = () => {
                 }
             }) 
         }),
-        body('password').notEmpty().withMessage('Please enter password').isLength({min: 8}).withMessage('Please enter at least 8 char password')
+        check('password').isLength({min: 8}).withMessage('Please enter at least 8 char password')
     ]
 };
